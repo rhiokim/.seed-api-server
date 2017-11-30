f35eba84948eda7db9ec03c39a9c2d0709080d4b
rhiokim <rhio.kim@gmail.com>
2017-11-30 13:06:47 +0900

    feat: add dev, build, and babeling

---
+++ dist/routes/sample.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.route('/').get((req, res, next) => {
+  res.send('success');
+});
+
+router.get('/:id', (req, res) => {
+  res.json(req.params);
+});
+
+module.exports = router;
